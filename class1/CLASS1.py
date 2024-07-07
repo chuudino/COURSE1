0be5cb047d42bfee87d72a772d1749244d867fbf
chuudino <chuudino@gmail.com>
2024-07-07 16:10:32 +0800

    PRACTICE 2

--- class1/CLASS1.py
+++ class1/CLASS1.py
@@ -1,1 +1,13 @@
 print("hello")
+print(1 + 2)  # practise add function
+print(3 - 1)  # practise sub function
+print(4 * 2)  # practise mul function
+print(5 / 2)  # practise div function
+print(6**2)  # practise pow function
+print(7 % 2)  # practise mod function
+print(8 // 2)  # practise floordiv function
+pi = 3.14
+print(pi)  # practise variable
+r = 3
+area = pi * r**2
+print(area)  # practise variable

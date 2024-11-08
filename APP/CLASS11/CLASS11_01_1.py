fc1cc4dbc0a712a81b94c8d35b0baf08ba7fb448
chuudino <chuudino@gmail.com>
2024-11-08 18:05:47 +0800

    Additional Ex 11.01

--- APP/CLASS11/CLASS11_01_1.py
+++ APP/CLASS11/CLASS11_01_1.py
@@ -7,13 +7,18 @@ os.chdir(sys.path[0])  # 設定工作目錄
 
 
 #######################定義函數########################
-def show_result():
-    check1.configure(text=str(check_type.get()))
+def show1_result():
+    check1.configure(text=str(check1_type.get()))
+
+
+def show2_result():
+    check2.configure(text=str(check2_type.get()))
 
 
 #######################建立視窗########################
 window = tk.Tk()
-window.title("COURSE 10_01")  # 設定視窗標題
+window.title("COURSE 10_01_1")  # 設定視窗標題
+window.geometry("500x200")
 
 #######################設定字型########################
 font_size = 26
@@ -23,20 +28,33 @@ window.option_add("*Font", f"TimesNewRoman {font_size}")
 style = Style(theme="simplex")
 style.configure("my.TCheckbutton", font=(f"TimesNewRoman {font_size}"))
 
-check_type = BooleanVar()
-check_type.set(True)
+check1_type = BooleanVar()
+check1_type.set(True)
 
+check2_type = BooleanVar()
+check2_type.set(True)
 ######################建立check button########################
 check1 = Checkbutton(
     window,
-    text=str(check_type.get()),
+    text=str(check1_type.get()),
     style="my.TCheckbutton",
-    variable=check_type,
-    command=show_result,
+    variable=check1_type,
+    command=show1_result,
     onvalue=True,
     offvalue=False,
 )
 check1.grid(row=0, column=0, padx=10, pady=10, sticky="NEWS")
+
+check2 = Checkbutton(
+    window,
+    text=str(check2_type.get()),
+    style="my.TCheckbutton",
+    variable=check2_type,
+    command=show2_result,
+    onvalue=True,
+    offvalue=False,
+)
+check2.grid(row=0, column=1, padx=10, pady=10, sticky="NEWS")
 
 ######################運行應用程式########################
 window.mainloop()  # 進入主迴圈

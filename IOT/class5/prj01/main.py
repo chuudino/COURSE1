16cbf72dbf764f55befefab85853e57371b664f6
chuudino <chuudino@gmail.com>
2025-05-09 15:41:20 +0800

    feat(gpio): 新增 gpio 類別以控制 GPIO 腳位並保護腳位編號
    feat(wifi): 新增 wifi 類別以管理 WiFi 連線與設定

---
+++ IOT/class5/prj01/main.py
@@ -0,0 +1,21 @@
+from machine import Pin, ADC  # 匯入控制腳位與類比輸入的模組
+from time import sleep  # 匯入延遲時間的 sleep 函式
+import mcu  # 匯入自訂的 mcu 模組
+import time  # 匯入時間相關模組
+
+
+# 建立 mcu.gpio 物件，方便後續控制腳位
+gpio = mcu.gpio()
+# 請替換成您的 WIFI 認證資訊
+WIFI_SSID = "Singular_AI"
+WIFI_PASSWORD = "Singular#1234"
+
+# 建立 mcu.wifi 物件
+wifi = mcu.wifi(WIFI_SSID, WIFI_PASSWORD)
+wifi.setup(sta_activate=True, ap_activate=False)
+wifi.scan()
+wifi.connect()
+
+# 主程式迴圈，目前尚未執行其他功能
+while True:
+    pass
